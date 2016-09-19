565887ff7576f866dafdc7977586311e9a4b7dae
Adam Furbee <ajf023@shsu.edu>
2016-09-19 14:16:40 -0500

    Added package for bencode libary

---
+++ bencode/bencode.py
@@ -0,0 +1,52 @@
+'''
+Bencoding
+
+Bencoding is a way to specify and organize data in a terse format. It supports the following types: byte strings, integers, lists, and dictionaries.
+Byte Strings
+
+Byte strings are encoded as follows: <string length encoded in base ten ASCII>:<string data>
+Note that there is no constant beginning delimiter, and no ending delimiter.
+
+    Example: 4: spam represents the string "spam"
+    Example: 0: represents the empty string ""
+
+Integers
+
+Integers are encoded as follows: i<integer encoded in base ten ASCII>e
+The initial i and trailing e are beginning and ending delimiters.
+
+    Example: i3e represents the integer "3"
+    Example: i-3e represents the integer "-3"
+
+i-0e is invalid. All encodings with a leading zero, such as i03e, are invalid, other than i0e, which of course corresponds to the integer "0".
+
+    NOTE: The maximum number of bit of this integer is unspecified, but to handle it as a signed 64bit integer is mandatory to handle "large files" aka .torrent for more that 4Gbyte.
+
+Lists
+
+Lists are encoded as follows: l<bencoded values>e
+The initial l and trailing e are beginning and ending delimiters. Lists may contain any bencoded type, including integers, strings, dictionaries, and even lists within other lists.
+
+    Example: l4:spam4:eggse represents the list of two strings: [ "spam", "eggs" ]
+    Example: le represents an empty list: []
+
+Dictionaries
+
+Dictionaries are encoded as follows: d<bencoded string><bencoded element>e
+The initial d and trailing e are the beginning and ending delimiters. Note that the keys must be bencoded strings. The values may be any bencoded type, including integers, strings, lists, and other dictionaries.
+Keys must be strings and appear in sorted order (sorted as raw strings, not alphanumerics). The strings should be compared using a binary comparison, not a culture-specific "natural" comparison.
+
+    Example: d3:cow3:moo4:spam4:eggse represents the dictionary { "cow" => "moo", "spam" => "eggs" }
+    Example: d4:spaml1:a1:bee represents the dictionary { "spam" => [ "a", "b" ] }
+    Example: d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee represents { "publisher" => "bob", "publisher-webpage" => "www.example.com", "publisher.location" => "home" }
+    Example: de represents an empty dictionary {}
+'''
+
+class Bencode(object):
+    '''
+    All strings are expected to be byte strings. ie: b'test' not 'test'
+    '''
+    def encode(data):
+        pass
+    def decode(data):
+        pass
---
+++ bencode/test_bencode.py
@@ -0,0 +1,184 @@
+import unittest
+import bencode
+
+class TestEncode(unittest.TestCase):
+
+    def test_string(self):
+        """ Test strings are encoded correctly. """
+
+        self.n = bencode.Bencode.encode("test")
+        self.assertEqual(self.n, b"4:test")
+
+    def test_empty__string(self):
+        """ Test empty strings are encoded correctly. """
+    
+        self.n = bencode.Bencode.encode("")
+        self.assertEqual(self.n, b"0:")
+
+    def test_integer(self):
+        """ Test integers are encoded correctly. """
+
+        self.n = bencode.Bencode.encode(42)
+        self.assertEqual(self.n, b"i42e")
+
+    def test_negative_integer(self):
+        """ Test negative integers are encoded correctly. """
+
+        self.n = bencode.Bencode.encode(-42)
+        self.assertEqual(self.n, b"i-42e")
+
+    def test_zero(self):
+        """ Test zero is encoded correctly. """
+
+        self.n = bencode.Bencode.encode(0)
+        self.assertEqual(self.n, b"i0e")
+
+    # @unittest.skip("To be implemented later")
+    def test_negative_zero(self):
+        """ Test encoding negative zero raises an exception. """
+
+        with self.assertRaises(ValueError):
+            bencode.Bencode.encode("i-0e")
+
+    def test_dict(self):
+        """ Test dictionaries are encoded correctly. """
+
+        self.n = bencode.Bencode.encode({b"bar": b"spam", b"foo": 42})
+        self.assertEqual(self.n, b"d3:bar4:spam3:fooi42ee")
+
+    @unittest.skip("To be implemented later")
+    def test_dict_keys_are_ordered(self):
+        """ Test dictionary keys are in sorted order. """
+        pass
+
+    def test_empty_dict(self):
+        """ Test empty dictionaries are encoded correctly. """
+
+        self.n = bencode.Bencode.encode({})
+        self.assertEqual(self.n, b"de")
+
+    def test_nested_dicts(self):
+        """ Test nested dictionaries are encoded correctly. """
+
+        self.n = bencode.Bencode.encode({b"":{b"":b"test"}})
+        self.assertEqual(self.n, b"d0:d0:4:testee")
+
+    def test_list(self):
+        """ Test lists are encoded correctly. """
+
+        self.n = bencode.Bencode.encode([b"spam", b"eggs", 3])
+        self.assertEqual(self.n, b"l4:spam4:eggsi3ee")
+
+    def test_empty_list(self):
+        """ Test empty lists are encoded correctly. """
+
+        self.n = bencode.Bencode.encode([])
+        self.assertEqual(self.n, b"le")
+
+    def test_nested_lists(self):
+        """ Test nested lists are encoded correctly. """
+
+        self.n = bencode.Bencode.encode([b"spam", b"eggs", [3]])
+        self.assertEqual(self.n, b"l4:spam4:eggsli3eee")
+
+    def test_list_inside_dict(self):
+        """ Test lists inside dictionaries are encoded correctly. """
+
+        self.n = bencode.Bencode.encode({b"spam":[b"a", b"b"]})
+        self.assertEqual(self.n, b"d4:spaml1:a1:bee")
+
+    def test_dict_inside_list(self):
+        """ Test dictionaries inside lists are encoded correctly. """
+
+        self.n = bencode.Bencode.encode([{b"spam":[b"a", b"b"]}])
+        self.assertEqual(self.n, b"ld4:spaml1:a1:beee")
+
+
+class TestDecode(unittest.TestCase):
+
+    def test_string(self):
+        """ Test strings are decoded correctly. """
+    
+        self.n = bencode.Bencode.decode(b"4:test")
+        self.assertEqual(self.n, b"test")
+
+    def test_empty_string(self):
+        """ Test empty strings are decoded correctly. """
+    
+        self.n = bencode.Bencode.decode(b"0:")
+        self.assertEqual(self.n, b"")
+
+    def test_integer(self):
+        """ Test integers are decoded correctly. """
+
+        self.n = bencode.Bencode.decode(b"i42e")
+        self.assertEqual(self.n, 42)
+
+    # @unittest.skip("To be implemented later")
+    def test_integer_for_leading_zeros(self):
+        """ Test integers with a leading zero raise an exception. """
+
+        with self.assertRaises(ValueError):
+            bencode.Bencode.decode(b"i042e")
+
+    def test_negative_integer(self):
+        """ Test negative integers are decoded correctly. """
+
+        self.n = bencode.Bencode.decode(b"i-42e")
+        self.assertEqual(self.n, -42)
+
+    def test_zero(self):
+        """ Test zero is decoded correctly. """
+
+        self.n = bencode.Bencode.decode(b"i0e")
+        self.assertEqual(self.n, 0)
+    def test_dict(self):
+        """ Test dictionaries are decoded correctly. """
+
+        self.n = bencode.Bencode.decode(b"d3:bar4:spam3:fooi42ee")
+        self.assertEqual(self.n, {b"bar": b"spam", b"foo": 42})
+
+    def test_empty_dict(self):
+        """ Test empty dictionaries are decoded correctly. """
+
+        self.n = bencode.Bencode.decode("de")
+        self.assertEqual(self.n, {})
+
+    def test_nested_dicts(self):
+        """ Test nested dictionaries are decoded correctly. """
+
+        self.n = bencode.Bencode.decode(b"d0:di42e4:testee")
+        self.assertEqual(self.n, {b"":{42:b"test"}})
+
+    def test_list(self):
+        """ Test lists are decoded correctly. """
+
+        self.n = bencode.Bencode.decode(b"l4:spam4:eggsi3ee")
+        self.assertEqual(self.n, [b"spam", b"eggs", 3])
+
+    def test_empty_list(self):
+        """ Test empty lists are decoded correctly. """
+
+        self.n = bencode.Bencode.decode("le")
+        self.assertEqual(self.n, [])
+
+    def test_nested_lists(self):
+        """ Test nested lists are decoded correctly. """
+
+        self.n = bencode.Bencode.decode("l4:spam4:eggsli3eee")
+        self.assertEqual(self.n, [b"spam", b"eggs", [3]])
+
+    def test_list_inside_dict(self):
+        """ Test lists inside dictionaries are decoded correctly. """
+
+        self.n = bencode.Bencode.decode("d4:spaml1:a1:bee")
+        self.assertEqual(self.n, {b"spam":[b"a", b"b"]})
+
+    def test_dict_inside_list(self):
+        """ Test dictionaries inside lists are decoded correctly. """
+
+        self.n = bencode.Bencode.decode("ld4:spaml1:a1:beee")
+        self.assertEqual(self.n, [{b"spam":[b"a", b"b"]}])
+
+if __name__ == '__main__':
+    unittest.main()
