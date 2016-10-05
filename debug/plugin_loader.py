0a34fa393e4208bb18fae3cceb04efa1980a664b
Adam Furbee <adam.furbee@gmail.com>
2016-10-05 17:01:29 -0500

    Added plugin_loader.py outline and description

---
+++ debug/plugin_loader.py
@@ -0,0 +1,7 @@
+"""
+This file is responsible for loading plugins listed in the database.
+The database is expected to hold the URL of the location of the plugin.
+"""
+
+class Loader(object)
+    pass
