d8974b2cf40c6dcdde8388abee63cffade0c724b
Adam Furbee <adam.furbee@gmail.com>
2016-10-05 16:53:56 -0500

    Added placeholders for the rest of the objects in the design along with descriptions

--- debug/db.py
+++ debug/db.py
@@ -3,5 +3,5 @@ This module will handle establishing the connection to the database
 and pass the connection to objects that use it.
 """
 
-class DBConnector(object):
+class Connector(object):
     pass
