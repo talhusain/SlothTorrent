fe5355036ac36fa86ee88ca6a9e14e41208c34cc
Adam <adam.furbee@gmail.com>
2016-10-19 21:57:22 -0500

    Updated bencoding, added sample torrents for the quick_torrentdriver for testing purposes.

--- debug/quick_tordriver.py
+++ debug/quick_tordriver.py
@@ -2,53 +2,71 @@
 
 # Requirements: bencoder & requests
 #   - can be gotten via 'pip install bencoder requests'
-from bencoder import encode, decode
+from bencoding.bencode import encode, decode
 import urllib
 import hashlib
 import requests
+import socket
+import random
+import os
+from struct import unpack
+from string import ascii_letters, digits
+
+
+import pprint
+pp = pprint.PrettyPrinter(indent=2)
 
 # GLOBALS:
-torrent_path = 'B:/learning/SHSU/00_fall-2016/software-engineering/project/debug/res/torrent-files/[a4e]Paranoia_Agent_01-13.torrent'
+torrent_path = '/Users/afurbee/Downloads/X-Men- Apocalypse (2016) [1080p] [YTS.AG].torrent'
 
-# Open torrent and read data into data variable
-data = None
-with open(torrent_path, 'rb') as f:
-    read_data = f.read()
-    data = decode(read_data)
 
-# Generate hash of info field
-info_hash = hashlib.sha1(encode(data[b'info'])).digest()
 
-# Generate static peer_id, just for PoC
-peer_id = bytes(range(20))
+def get_peer_list(torrent_path):
+    # Open torrent and read data into data variable
+    data = None
+    with open(torrent_path, 'rb') as f:
+        read_data = f.read()
+        data = decode(read_data)
 
-# Calculate length of all torrent files, assume these are all multi-torrent files
-length = 0
+    # Generate hash of info field
+    info_hash = hashlib.sha1(encode(data[b'info'])).digest()
+    
+    # Generate static peer_id, just for PoC
+    VERSION = '0001'
+    ALPHANUM = ascii_letters + digits
+    peer_id = ('-DR' + VERSION + ''.join(random.sample(ALPHANUM, 13)))
+    
+    # Calculate length of all torrent files, assume these are all multi-torrent files
+    length = 0
+    for file in data[b'info'][b'files']:
+        length += file[b'length']
 
-print('\n')
-print('\tLength:')
-for file in data[b'info'][b'files']:
-    print('\t' + str(file[b'length']))
-    length += file[b'length']
+    # Make requests to a bunch of trackers and get the list of peers
+    decoded_peers = []
+    for tracker in data[b'announce-list']:
+        payload = {'info_hash': info_hash, 'peer_id': peer_id, 'length': length,
+                    'port': 6881, 'uploaded':0, 'downloaded':0, 'compact':1}
+        try:
+            print('Attempting connection to:')
+            print('http://' + tracker[0].decode("utf-8")[6:])
+            r = requests.get('http://' + tracker[0].decode("utf-8")[6:], params=payload, timeout=3)
+            if(r.status_code == 200):
+                # print('http://' + tracker[0].decode("utf-8")[6:])
+                # Grab the peers and split them into manageable chunks
+                peers = decode(r.content)[b'peers']
+                peers = [peers[i:i+6] for i in range(0, len(peers), 6)]
+                # For each peer: print the ip and unpack the last 2 bytes as an int for the port
+                for p in peers:
+                    ip = str(socket.inet_ntoa(p[:4]))
+                    port = str(unpack(">H", p[4:])[0])
+                    decoded_peers.append((ip, port))
+        except Exception as e:
+            print(e)
+            pass
+    pp.pprint(decoded_peers)
+    return(decoded_peers)
 
-# Make requests to a bunch of trackers
-if not b'nodes' in data:
-    if (b'announce'):
-        #print('\n' + str(data) + '\n')
-        for item in data:
-            if (item == b'info'):
-                for sub in item:
-                    print(sub)
-            else:
-                print(item)
-    elif (b'announce-list'):
-        for tracker in data[b'announce-list']:
-            payload = {'info_hash': info_hash, 'peer_id': peer_id.decode("utf-8"), 'length': length,
-                        'port': 6881, 'uploaded':0, 'downloaded':0, 'compact':1}
-            try:
-                print('\n' + 'http://' + tracker[0].decode("utf-8")[6:])
-                r = requests.get('http://' + tracker[0].decode("utf-8")[6:], params=payload, timeout=3)
-                print(r.url)
-                print(r.content)
-            except:
-                pass
+for file in os.listdir('sample_torrents'):
+    print('getting list of peers for ' + file)
+    get_peer_list(torrent_path)
+    print('========================================================================================')
