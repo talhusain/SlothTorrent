6ed8a9b0b6245c405bc051a9b360368f43083810
Adam Furbee <adam.furbee@gmail.com>
2016-11-17 12:16:35 -0600

    Updated index page to display recent torrents, other minor bug fixes included

--- debug/controllers/index.py
+++ debug/controllers/index.py
@@ -3,30 +3,35 @@ Controller will be responsible for serving the index/search main page.
 It is expects a query and returns a list of torrents.
 """
 
-from flask import Flask, Blueprint, render_template, request, redirect, url_for
+# from flask import Flask
+from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 index_page = Blueprint('index_page', __name__)
 
 db = None
 
+
 class IndexController(object):
     def __init__(self, database):
+        global db
         db = database
+
 
 @index_page.route('/')
 def index():
-    return render_template('index.html')
+    torrents = db.get_recent_torrents()
+    print(torrents)
+    return render_template('index.html', torrents=torrents)
+
 
 @index_page.route('/hello/')
 @index_page.route('/hello/<name>')
 def hello(name=None):
     return render_template('index.html', name=name)
-
-
-
-
-
-
 
 
 @index_page.route('/login', methods=['GET', 'POST'])
