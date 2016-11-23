50cbcf9d4825f6830ea2a92bf9f6cab1d22d43a3
Adam Furbee <adam.furbee@gmail.com>
2016-11-23 10:12:19 -0600

    Added base function definitions for URI handlers

--- debug/controllers/index.py
+++ debug/controllers/index.py
@@ -1,42 +1,26 @@
-"""
-Controller will be responsible for serving the index/search main page.
-It is expects a query and returns a list of torrents.
-"""
-
-from flask import Flask, Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
 
 index_page = Blueprint('index_page', __name__)
-
 db = None
+
 
 class IndexController(object):
     def __init__(self, database):
+        global db
         db = database
 
-@index_page.route('/')
+
+@index_page.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+
+# left for testing purposes
 @index_page.route('/hello/')
 @index_page.route('/hello/<name>')
 def hello(name=None):
     return render_template('index.html', name=name)
-
-
-
-
-
-
-
-
-@index_page.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        return redirect(url_for('index_page.hello', name=username))
-    return '''
-        <form action="" method="post">
-            <p><input type=text name=username>
-            <p><input type=submit value=Login>
-        </form>
-    '''
--- debug/request_handler.py
+++ debug/request_handler.py
@@ -6,11 +6,15 @@ setup requests that will be expected
 from flask import Flask
 from flask import send_from_directory
 from controllers.index import IndexController, index_page
-from controllers.torrent import torrent_page
+from controllers.torrent import TorrentController, torrent_page
+from controllers.admin import AdminController, admin_page
+from controllers.login import LoginController, login_page
 
 app = Flask(__name__, static_url_path='/static')
 app.register_blueprint(index_page)
 app.register_blueprint(torrent_page)
+app.register_blueprint(admin_page)
+app.register_blueprint(login_page)
 
 
 class RequestHandler(object):
@@ -18,11 +22,10 @@ class RequestHandler(object):
     def __init__(self, db):
         self.db = db
         self.index_controller = IndexController(db)
+        self.login_controller = LoginController(db)
+        self.admin_controller = AdminController(db)
+        self.torrent_controller = TorrentController(db, None)
         app.run()
-
-    @app.route('/admin')
-    def admin_page():
-        return "Hello, world! This is the admin page"
 
 
 # serve static files from flask for dev purposes, nginx will be
