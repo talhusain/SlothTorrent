0290a629efd49283d27c17b42c946ff2a88cd5c4
James Morse <jms.r.mors@gmail.com>
2016-11-28 20:13:54 -0600

    Updated test_bitfield__to_bytes() and test_request__to_bytes()

--- debug/TorrentClient/test_get_message.py
+++ debug/TorrentClient/test_get_message.py
@@ -111,8 +111,8 @@ class TestGetMessage(unittest.TestCase):
     	self.assertEqual(isinstance(msg, BitField), True)
 
     def test_bitfield__to_bytes(self):
-        bitfield_str = '1111111011111111'
-        bitfield_b = b'\x00\x00\x00\x11\x05\x01\x01\x01\x01\x01\x01\x01\x00\x01\x01\x01\x01\x01\x01\x01\x01'
+        bitfield_str = [2 * 4, 2 * 8]
+        bitfield_b = b'\x00\x00\x00\x03\x05\x08\x10'
         msg_b = Message.get_message('bitfield', None, None, None, None, None, bitfield_str).to_bytes()
         self.assertEqual(msg_b, bitfield_b)
 
@@ -136,8 +136,9 @@ class TestGetMessage(unittest.TestCase):
     	self.assertEqual(msg, Request(0, 1, 2 ** 8))
 
     def test_request__to_bytes(self):
-        bitfield_str = '1111111011111111'
-        bitfield_b = b'\x00\x00\x00\x11\x05\x01\x01\x01\x01\x01\x01\x01\x00\x01\x01\x01\x01\x01\x01\x01\x01'
+        #bitfield_str = '1111111011111111'
+        bitfield_str = [2 * 4, 2 * 8]
+        bitfield_b = b'\x00\x00\x00\x03\x05\x08\x10'
         msg_b = Message.get_message('bitfield', None, None, None, None, None, bitfield_str).to_bytes()
         self.assertEqual(msg_b, bitfield_b)
 
