73e03c2e631ee022cd51cc714c5b81e79d56fe49
Adam Furbee <adam.furbee@gmail.com>
2016-11-29 14:47:34 -0600

    Updated template on torrent page

--- debug/TorrentClient/client.py
+++ debug/TorrentClient/client.py
@@ -29,7 +29,7 @@ class Client(object):
             self.download_location = 'torrent_downloads/'
         else:
             self.download_location = download_location
-        threading.Timer(60, self._keepalive_peers).start()
+        threading.Timer(10, self._keepalive_peers).start()
 
     def start(self, torrent):
         torrent.status = 'downloading'
